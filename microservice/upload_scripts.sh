2061eafef74500626b9089653714f34a542be27c
Arin Trongsantipong <arintrongs@gmail.com>
2019-03-26 20:13:56 +0700

    add bulk upload scripts

---
+++ microservice/upload_scripts.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sshpass -p 'PrinceTyo;oyomN5575' scp /Users/tomfoolery/Documents/Work/Thaidata/tdh-monitor-backend/microservice/load.php root@cloud-linux01.thaidatahosting.com:/var/www/html/
+sshpass -p 'PrinceTyo;oyomN5575' scp /Users/tomfoolery/Documents/Work/Thaidata/tdh-monitor-backend/microservice/load.php root@cloud-linux05.thaidatahosting.com:/var/www/html/
+sshpass -p 'PrinceTyo;oyomN5575' scp /Users/tomfoolery/Documents/Work/Thaidata/tdh-monitor-backend/microservice/load.php root@cloud-linux07.thaidatahosting.com:/var/www/html/
+sshpass -p 'PrinceTyo;oyomN5575' scp /Users/tomfoolery/Documents/Work/Thaidata/tdh-monitor-backend/microservice/load.php root@cloud-linux09.thaidatahosting.com:/var/www/html/
+sshpass -p 'PrinceTyo;oyomN5575' scp /Users/tomfoolery/Documents/Work/Thaidata/tdh-monitor-backend/microservice/load.php root@cloud-linux11.thaidatahosting.com:/var/www/html/
+sshpass -p 'PrinceTyo;oyomN5575' scp /Users/tomfoolery/Documents/Work/Thaidata/tdh-monitor-backend/microservice/load.php root@cloud-linux15.thaidatahosting.com:/var/www/html/
+sshpass -p 'PrinceTyo;oyomN5575' scp /Users/tomfoolery/Documents/Work/Thaidata/tdh-monitor-backend/microservice/load.php root@cloud-linux101.thaidatahosting.com:/var/www/html/
+sshpass -p 'PrinceTyo;oyomN5575' scp /Users/tomfoolery/Documents/Work/Thaidata/tdh-monitor-backend/microservice/load.php root@cloud-linux102.thaidatahosting.com:/var/www/html/
